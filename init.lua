ec41785d859e68c60c2cc65b4e3389eab9f8b11a
danknil <danknil@protonmail.com>
2024-07-25 19:35:17 +0700

    change colorscheme

--- init.lua
+++ init.lua
@@ -1,21 +1,21 @@
 -- gruvbox material light medium, you can change it to your needs
 _G.colorscheme = {
-    base00 = '#fbf1c7',
-    base01 = '#f2e5bc',
-    base02 = '#d5c4a1',
-    base03 = '#bdae93',
-    base04 = '#665c54',
-    base05 = '#654735',
-    base06 = '#3c3836',
-    base07 = '#282828',
-    base08 = '#c14a4a',
-    base09 = '#c35e0a',
-    base0A = '#b47109',
-    base0B = '#6c782e',
-    base0C = '#4c7a5d',
-    base0D = '#45707a',
-    base0E = '#945e80',
-    base0F = '#e78a4e',
+    base00 = '#ECEDF3',
+    base01 = '#CBCCD1',
+    base02 = '#DFE0E5',
+    base03 = '#9699A3',
+    base04 = '#4C505E',
+    base05 = '#4C505E',
+    base06 = '#1A1B26',
+    base07 = '#1A1B26',
+    base08 = '#8C4351',
+    base09 = '#965027',
+    base0A = '#8F5E15',
+    base0B = '#33635C',
+    base0C = '#0F4B6E',
+    base0D = '#34548A',
+    base0E = '#5A4A78',
+    base0F = '#655259',
 }
 -- setting up vanilla settings
 require 'autocmds'
@@ -39,4 +39,3 @@ require('lazy').setup 'plugins'
 
 -- setting up vanilla binds
 require('mappings'):load_mappings('general', { noremap = true })
-
