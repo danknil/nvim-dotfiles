bb2ef64d83f1725cf3e9f47617d512c23ea85c7d
Mikhail Balashov <danknil@protonmail.com>
2025-07-29 14:53:10 +0700

    commented mkdir autocmd as for now

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -4,16 +4,16 @@ local autocmd = vim.api.nvim_create_autocmd
 local disable_ft = { 'TelescopePrompt', 'netrw', 'zsh', 'oil' }
 
 -- auto mkdir
-augroup('mkdir', { clear = true })
-autocmd('BufWritePre', {
-    group = 'mkdir',
-    pattern = '*',
-    callback = function()
-        local file_path = vim.fn.expand('%:p')
-        local dir_path = vim.fn.fnamemodify(file_path, ':h')
-
-        if not vim.loop.fs_stat(dir_path) then
-            vim.fn.mkdir(dir_path, 'p')
-        end
-    end
-})
+-- augroup('mkdir', { clear = true })
+-- autocmd('BufWritePre', {
+--     group = 'mkdir',
+--     pattern = '*',
+--     callback = function()
+--         local file_path = vim.fn.expand('%:p')
+--         local dir_path = vim.fn.fnamemodify(file_path, ':h')
+--
+--         if not vim.loop.fs_stat(dir_path) then
+--             vim.fn.mkdir(dir_path, 'p')
+--         end
+--     end
+-- })
