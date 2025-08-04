d7d82b9f2a1c74cb26082a4a69efe270e2641a93
Mikhail Balashov <danknil@protonmail.com>
2025-08-04 15:05:34 +0700

    added some options

--- init.lua
+++ init.lua
@@ -15,7 +15,11 @@ o.relativenumber = true
 o.numberwidth = 2
 
 o.signcolumn = 'yes'
+
+o.expandtab = true
+o.softtabstop = 4
 o.tabstop = 4
+o.shiftwidth = 4
 
 o.timeoutlen = 400
 o.swapfile = false
@@ -23,10 +27,16 @@ o.undofile = true
 o.scrolloff = 8
 o.sidescrolloff = 8
 
+o.foldexpr = 'nvim_treesitter#foldexpr()'
+o.foldmethod = 'expr'
+o.foldlevelstart = 99
+
 o.list = true
 o.listchars = { eol = '↲', tab = '▎ ' }
 
 o.termguicolors = true
+
+o.winborder = 'bold'
 
 require 'autocmds'
 require 'plugins'
@@ -40,39 +50,39 @@ vim.cmd [[
 ]]
 
 utils.map({
-	n = {
-		['<leader>e'] = function()
-			local bufdir = utils.get_curr_bufdir()
-			return ':e ' .. bufdir
-		end,
-	},
+    n = {
+        ['<leader>e'] = function()
+            local bufdir = utils.get_curr_bufdir()
+            return ':e ' .. bufdir
+        end,
+    },
 }, { noremap = true, expr = true })
 
 utils.map({
-	n = {
-		['<space>'] = '<cmd>noh<cr>',
-		['<C-c>'] = '<cmd>%y+<cr>',
-		['<Tab>'] = '<cmd>bn<cr>',
-		['<S-Tab>'] = '<cmd>bp<cr>',
-		['<leader>bd'] = '<cmd>bdelete',
-		['<leader>y'] = '"+y',
-		['<leader>Y'] = '"+Y',
-		['<leader>p'] = '"+p',
-		['<leader>P'] = '"+P',
-		['<leader>d'] = '"+d',
-	},
-	v = {
-		['<leader>y'] = '"+y',
-		['<leader>Y'] = '"+Y',
-		['<leader>p'] = '"+p',
-		['<leader>P'] = '"+P',
-		['<leader>d'] = '"+d',
-	},
-	x = {
-		['<leader>y'] = '"+y',
-		['<leader>Y'] = '"+Y',
-		['<leader>p'] = '"+p',
-		['<leader>P'] = '"+P',
-		['<leader>d'] = '"+d',
-	},
+    n = {
+        ['<space>'] = '<cmd>noh<cr>',
+        ['<C-c>'] = '<cmd>%y+<cr>',
+        ['<Tab>'] = '<cmd>bn<cr>',
+        ['<S-Tab>'] = '<cmd>bp<cr>',
+        ['<leader>bd'] = '<cmd>bdelete',
+        ['<leader>y'] = '"+y',
+        ['<leader>Y'] = '"+Y',
+        ['<leader>p'] = '"+p',
+        ['<leader>P'] = '"+P',
+        ['<leader>d'] = '"+d',
+    },
+    v = {
+        ['<leader>y'] = '"+y',
+        ['<leader>Y'] = '"+Y',
+        ['<leader>p'] = '"+p',
+        ['<leader>P'] = '"+P',
+        ['<leader>d'] = '"+d',
+    },
+    x = {
+        ['<leader>y'] = '"+y',
+        ['<leader>Y'] = '"+Y',
+        ['<leader>p'] = '"+p',
+        ['<leader>P'] = '"+P',
+        ['<leader>d'] = '"+d',
+    },
 }, { noremap = true, silent = true })
